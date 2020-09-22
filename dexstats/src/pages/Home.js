c75caa694bf1e608cfaf715c04996e721f3e181f
Michael Sanderson <jawm17@gmail.com>
2020-09-21 19:23:34 -0700

    Basic setup

---
+++ dexstats/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./homeStyle.css";
+
+export default function Home() {
+
+
+    return (
+        <div>
+            <div className="homeOuter">
+                <div className="header">
+                    <div className="title">
+                        dexstats.io
+                    </div>
+                </div>
+                <div className="exchanges">
+                    <div className="exchange">
+                    </div>
+                    <div className="exchange">
+                    </div>
+                    <div className="exchange">
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
