b7caa50e35c7529532de7365318cb506e5506579
Michael Sanderson <jawm17@gmail.com>
2020-09-23 15:27:59 -0700

    Semantics

--- dexstats/src/pages/Home.js
+++ dexstats/src/pages/Home.js
@@ -32,15 +32,19 @@ export default function Home() {
                 <div className="exchanges">
                     <div id="one" className="exchange">
                         <img id="uniswapIcon" className="icon" src="https://research.binance.com/static/images/projects/uniswap/logo.png" alt="uniswap icon"></img>
+                        Uniswap
                     </div>
                     <div className="exchange">
                         <img id="kyberIcon" className="icon" src="https://assets.coingecko.com/coins/images/947/large/kyber-logo.png?1547034806" alt="kyber icon"></img>
+                        Kyber Network
                     </div>
                     <div className="exchange">
                         <img id="zrxIcon" className="icon" src="https://assets.coingecko.com/coins/images/863/large/0x.png?1547034672" alt="zero x icon"></img>
+                        0x Protocol
                     </div>
                     <div className="exchange">
                         <img id="bancorIcon" className="icon" src="https://www.worldcryptoindex.com/wp-content/uploads/2018/06/bancor-logo.jpg" alt="bancor icon"></img>
+                        bancor
                     </div>
                     <div className="exchange">
                     </div>
