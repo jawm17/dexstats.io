54685bf968b45253d6bd721c417391d7fc1abab4
Michael Sanderson <jawm17@gmail.com>
2020-09-23 17:48:07 -0700

    STats for all yes stats for all thank you very much

--- dexstats/src/pages/Home.js
+++ dexstats/src/pages/Home.js
@@ -30,19 +30,23 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="exchanges">
+
+
+
+
                     <div id="one" className="exchange">
                         <div className="nameAndIcon">
                             <img id="uniswapIcon" className="icon" src="https://research.binance.com/static/images/projects/uniswap/logo.png" alt="uniswap icon"></img>
                             Uniswap
                         </div>
-                            <div id="volume1" className="volume">
-                                24hr Volume
-                                <p className="amount">$332,000,000</p>
-                            </div>
-                            <div id="liquidity1" className="liquidity">
-                                Total Liquidity
-                                <p className="amount">$1.92 B</p>
-                            </div>
+                        <div id="volume1" className="volume">
+                            24hr Volume
+                            <p className="amount">$332,000,000</p>
+                        </div>
+                        <div id="liquidity1" className="liquidity">
+                            Total Liquidity
+                            <p className="amount">$1.92 B</p>
+                        </div>
                     </div>
 
 
@@ -53,17 +57,65 @@ export default function Home() {
 
 
                     <div id="two" className="exchange">
-                        <img id="kyberIcon" className="icon" src="https://assets.coingecko.com/coins/images/947/large/kyber-logo.png?1547034806" alt="kyber icon"></img>
-                        Kyber Network
+                        <div className="nameAndIcon">
+                            <img id="kyberIcon" className="icon" src="https://assets.coingecko.com/coins/images/947/large/kyber-logo.png?1547034806" alt="kyber icon"></img>
+                            Kyber Network
+                        </div>
+                        <div id="volume2" className="volume">
+                            24hr Volume
+                                <p className="amount">$42,678,000</p>
+                        </div>
+                        <div id="liquidity2" className="liquidity">
+                            Total Liquidity
+                                <p className="amount">$200 M</p>
+                        </div>
                     </div>
+
+
+
+
+
+
+
                     <div className="exchange">
-                        <img id="zrxIcon" className="icon" src="https://assets.coingecko.com/coins/images/863/large/0x.png?1547034672" alt="zero x icon"></img>
-                        0x Protocol
+                        <div className="nameAndIcon">
+                            <img id="zrxIcon" className="icon" src="https://assets.coingecko.com/coins/images/863/large/0x.png?1547034672" alt="zero x icon"></img>
+                             0x Protocol
+                        </div>
+                        <div id="volume3" className="volume">
+                            24hr Volume
+                                <p className="amount">$332,000,000</p>
+                        </div>
+                        <div id="liquidity3" className="liquidity">
+                            Total Liquidity
+                                <p className="amount">$1.92 B</p>
+                        </div>
                     </div>
+
+
+
+
+
                     <div className="exchange">
-                        <img id="bancorIcon" className="icon" src="https://www.worldcryptoindex.com/wp-content/uploads/2018/06/bancor-logo.jpg" alt="bancor icon"></img>
-                        bancor
+                        <div className="nameAndIcon">
+                            <img id="bancorIcon" className="icon" src="https://www.worldcryptoindex.com/wp-content/uploads/2018/06/bancor-logo.jpg" alt="bancor icon"></img>
+                            bancor
+                        </div>
+                        <div id="volume4" className="volume">
+                            24hr Volume
+                                <p className="amount">$332,000,000</p>
+                        </div>
+                        <div id="liquidity4" className="liquidity">
+                            Total Liquidity
+                                <p className="amount">$1.92 B</p>
+                        </div>
                     </div>
+
+
+
+
+
+
                     <div className="exchange">
                     </div>
                     <div className="exchange">
