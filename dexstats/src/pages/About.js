c3382944dcb56dacbfa7984eebb7f36eba7d0bac
Michael Sanderson <jawm17@gmail.com>
2020-09-30 12:44:02 -0700

    Paragraph Styling

--- dexstats/src/pages/About.js
+++ dexstats/src/pages/About.js
@@ -2,27 +2,22 @@ import React, { useEffect, useState } from 'react';
 import "./aboutStyle.css";
 
 export default function About() {
-    const [header, setHeader] = useState("largeHeader");
 
     useEffect(() => {
         document.getElementById("container").setAttribute("style", "display:none");
-        document.addEventListener("scroll", () => {
-            let name = window.scrollY < 10 ? "largeHeader" : "smallHeader";
-            setHeader(name);
-        });
     }, []);
 
 
     return (
         <div>
             <div className="aboutPage">
-                <div className={header}>
+                <div className="largeHeader">
                     <div className="headerCenter">
                         <div className="title">
-                            dexstats.io
+                            About
                         </div>
-                        <div className="slogan">
-                            Decentralized exchange statistics
+                        <div className="subTitle">
+                            Dexstats.io
                         </div>
                         <div className="volumeTotal">
                             24hr volume
@@ -41,31 +36,19 @@ export default function About() {
                 <div className="aboutCenter">
                     <p>
                         Dexstats is here to provide you with the most accurate and up to date decentralized cryptocurrency exchange statistics.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu. Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Ut minim occaecat commodo dolor eu.
                     </p>
                     <br></br>
                     <br></br>
                     <p>
-                        Our website is made possible with data from the dexstats api. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu. Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Ut minim occaecat commodo dolor eu.
+                        Our website is made possible with data from the Dexstats <a href="/data">api</a>.
                     </p>
                     <br></br>
                     <br></br>
-                    <p>
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu. Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu. Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Elit culpa veniam tempor eu magna id ullamco labore. Ut minim occaecat commodo dolor eu.
-                        Sunt ullamco est ea deserunt qui nisi labore eiusmod consequat. Ut minim occaecat commodo dolor eu.
+                    <p className="titleAPI">
+                        Dexstats API
                     </p>
+                    <br></br>
+                    <br></br>
                 </div>
             </div>
         </div>
