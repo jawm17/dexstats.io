08b99158e16e7bc0509332349230cae5732347b8
Michael Sanderson <jawm17@gmail.com>
2020-10-13 20:47:59 -0700

    copyright protections

--- dexstats/src/App.js
+++ dexstats/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import ExchangePage from "./pages/ExchangePage";
 import About from "./pages/About";
 import Data from "./pages/Data";
+import Copyright from "./pages/Copyright";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/exchange/:exchange" component={ExchangePage} />
         <Route exact path="/about" component={About} />
         <Route exact path="/data" component={Data} />
+        <Route exact path="/copyright" component={Copyright} />
       </div>
     </Router>
   );
---
+++ dexstats/src/pages/Copyright.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./copyrightStyle.css";
+
+export default function Copyright() {
+
+    return (
+        <div className="copyrightOuter">
+            <h1>Copyright</h1>
+            <br></br>
+            <p>
+                Copyright dexstats.io 2020
+            </p>
+        </div>
+    );
+}
